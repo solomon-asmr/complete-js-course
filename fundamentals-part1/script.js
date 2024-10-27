36ddcfb8b08423d30cd17ca0884d6e36bce27cdf
solomon-asmr <selemonasmr7@gmail.com>
2024-10-27 22:13:05 +0200

    variables and values

--- fundamentals-part1/script.js
+++ fundamentals-part1/script.js
@@ -1,2 +1,22 @@
-let m='amazing';
-if(m==='amazing') alert("javascript is fun!!")
+let js='amazing';
+if(js==='amazing') alert("javascript is fun!!")
+console.log("jonas");
+
+let firstName = 'matilda';
+
+//if the varible you want to use is a key word in javascript you can add either $ or _ sign at the beginning of
+//the word
+let _function = "full stack developer"
+let $new = "internship"
+
+console.log(firstName+" "+_function+" "+$new);
+
+let PI = 3.1415;
+
+// proper way of naming variables
+let myFirstJob = "full stack developer";
+let myCurrentJob = "chief excutive manager";
+
+//incorrect way of naming variables
+let job1="full stack developer";
+let job2="ceo";
